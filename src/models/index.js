463740b362fcc4556a1ef0155b7ca27931d1925f
renatomak <renato.mark.silva@gmail.com>
2021-10-09 10:48:46 -0300

    method createModel

---
+++ src/models/index.js
@@ -0,0 +1,22 @@
+const connect = require('../../config/conn');
+const { ObjectId } = require('mongodb');
+
+const COLLECTION_NAME = 'users';
+
+const createModel = async ({ name, userName, password }) =>
+  connect().then(async (db) => {
+    const result = await db
+      .collection(COLLECTION_NAME)
+      .insertOne({ name, userName, password });
+    const user = {
+      _id: result.insertedId,
+      name,
+      userName,
+      dateAccess: new Date(),
+    };
+    return user;
+  });
+
+module.exports = {
+  createModel,
+};
