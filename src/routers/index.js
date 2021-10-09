dd5f22259f3374e69ae7ce2d874706d8ce1a7142
renatomak <renato.mark.silva@gmail.com>
2021-10-09 11:04:30 -0300

    method router /users

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { createUser } = require('../controllers');
 
 const router = express.Router();
+
+router.use('/users', createUser);
 
 module.exports = router;
