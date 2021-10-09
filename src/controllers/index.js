223cfc8412d6fc7d3606b3b2a8f4955130f22918
renatomak <renato.mark.silva@gmail.com>
2021-10-09 12:28:40 -0300

    It will be validated that the "userName" field is unique

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,21 +1,31 @@
 const rescue = require('express-rescue');
-const { STATUS_201_CREATED, STATUS_400_BAD_REQUEST } = require('../util');
+const {
+  STATUS_201_CREATED,
+  STATUS_400_BAD_REQUEST,
+  STATUS_409_CONFLICT,
+} = require('../util');
 const { createService } = require('../services');
-
 
 const createUser = rescue(async (req, res) => {
   try {
     const { name, userName, password } = req.body;
 
     const user = await createService({ name, userName, password });
+    console.log(user)
+    
+    if (user.registered) {
+      return res
+        .status(STATUS_409_CONFLICT)
+        .json({ message: 'User already registered!' });
+    }
 
     return res.status(STATUS_201_CREATED).json({ user });
   } catch (error) {
     console.error(error.message);
     return res
       .status(STATUS_400_BAD_REQUEST)
-      .json({ message: 'Invalid fields'});
+      .json({ message: 'Invalid fields' });
   }
 });
 
-module.exports = { createUser }
+module.exports = { createUser };
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,17 @@
-const { createModel } = require('../models');
+const { createModel, findUserNameModel } = require('../models');
 const { messageError } = require('../util');
 
 const createService = async (user) => {
   try {
+
+    const registered = await findUserNameModel(user.userName);
+
+    if (registered?.user) {
+      return { registered: true };
+    }
+
     const result = await createModel(user);
+
     return result;
   } catch (error) {
     throw Error(messageError(error.message, 'register users'));
