c250008b3b8b5f923b840dfcc205c16c6c2f4b80
renatomak <renato.mark.silva@gmail.com>
2021-10-09 12:03:23 -0300

    Test to validate userName format

--- src/tests/create.test.js
+++ src/tests/create.test.js
@@ -1,7 +1,10 @@
 const frisby = require('frisby');
 const { MongoClient } = require('mongodb');
-const { STATUS_201_CREATED, STATUS_400_BAD_REQUEST } = require('../util');
-
+const {
+  STATUS_201_CREATED,
+  STATUS_400_BAD_REQUEST,
+  STATUS_200_OK,
+} = require('../util');
 
 const mongoDbUrl = 'mongodb://localhost:27017/api-login';
 
@@ -29,14 +32,16 @@ describe('1 - Endpoint POST /users', () => {
   beforeEach(async () => {
     await db.collection('users').deleteMany({});
     const users = {
-      name: 'admin', userName: 'admin', password: 'admin' };
+      name: 'admin',
+      userName: 'admin',
+      password: 'admin',
+    };
     await db.collection('users').insertOne(users);
   });
 
   afterAll(async () => {
     await connection.close();
   });
-
 
   test('1.1 - Success case test in creating a new user', async () => {
     await frisby
@@ -49,8 +54,10 @@ describe('1 - Endpoint POST /users', () => {
       })
       .expect('status', STATUS_201_CREATED)
       .then((responseCreate) => {
-        const { json: { user } } = responseCreate;
- 
+        const {
+          json: { user },
+        } = responseCreate;
+
         expect(user.name).toEqual(postUserMock.user.name);
         expect(user.userName).toEqual(postUserMock.user.userName);
         expect(user.password).toEqual(postUserMock.user.password);
@@ -87,14 +94,58 @@ describe('1 - Endpoint POST /users', () => {
     await frisby
       .post(`${url}/users`, {
         name: 'user',
-        userName: 'user.user'
+        userName: 'user.user',
       })
       .expect('status', STATUS_400_BAD_REQUEST)
       .then((responseCreate) => {
         const { json } = responseCreate;
-        console.log(json)
         expect(json).toEqual({ message: 'The "password" field is mandatory' });
       });
   });
 
+  test('1.5 - Test to validate userName format', async () => {
+    await frisby
+      .post(`${url}/users`, {
+        name: 'user',
+        userName: 'user.user',
+        password: '123456',
+      })
+      .expect('status', STATUS_201_CREATED)
+      .then((responseCreate) => {
+        const {
+          json: { user },
+        } = responseCreate;
+        expect(user.userName).toEqual('user.user');
+      });
+
+    await frisby
+      .post(`${url}/users`, {
+        name: 'user',
+        userName: 'user user',
+        password: '123456',
+      })
+      .expect('status', STATUS_400_BAD_REQUEST)
+      .then((responseCreate) => {
+        const { json } = responseCreate;
+        expect(json).toEqual({
+          message:
+            'the username can then have letters, numbers or ".", "_", "-"',
+        });
+      });
+      
+    await frisby
+      .post(`${url}/users`, {
+        name: 'user',
+        userName: '@useruser',
+        password: '123456',
+      })
+      .expect('status', STATUS_400_BAD_REQUEST)
+      .then((responseCreate) => {
+        const { json } = responseCreate;
+        expect(json).toEqual({
+          message:
+            'the username can then have letters, numbers or ".", "_", "-"',
+        });
+      });
+  });
 });
